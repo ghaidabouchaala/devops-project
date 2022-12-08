697d6dc8bb2872d678602798d41dee70279a2c02
Ghaida Bouchaala <ghaida.bouchaala@expensya.com>
2022-12-08 16:58:20 +0100

    fix: nexus

--- script.groovy
+++ script.groovy
@@ -1,7 +1,7 @@
 def pushToNexus() {
     echo "pushing the jar file to Nexus maven-snapshots repo..."
     bat 'mvn clean install -D maven.test.skip=true'
-    nexusArtifactUploader artifacts: [[artifactId: 'demo', classifier: '', file: 'target/demo-0.0.1-SNAPSHOT.jar', type: 'jar']], credentialsId: 'nexus-credentials', groupId: 'com.example', nexusUrl: 'http://localhost:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshots', version: '0.0.1-SNAPSHOT'
+    nexusArtifactUploader artifacts: [[artifactId: 'demo', classifier: '', file: 'target/demo-0.0.1-SNAPSHOT.jar', type: 'jar']], credentialsId: 'nexus-credentials', groupId: 'com.example', nexusUrl: 'localhost:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshots', version: '0.0.1-SNAPSHOT'
 }
 
 def sonarScan() {
