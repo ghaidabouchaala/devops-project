679975929377099d831c0847bc7f300d13670612
Ghaida Bouchaala <ghaida.bouchaala@expensya.com>
2022-12-08 14:33:10 +0100

    fix: nexus path

--- script.groovy
+++ script.groovy
@@ -1,6 +1,7 @@
 def pushToNexus() {
     echo "pushing the jar file to Nexus maven-snapshots repo..."
     bat 'mvn clean package -D maven.test.skip=true deploy:deploy-file -D groupId=com.example -D artifactId=demo -D version=3.0.0 -D packaging=jar -D repositoryId=deploymentRepo -D url=http://localhost:8081/repository/maven-releases/ -D file=target/demo-0.0.1-SNAPSHOT.jar'
+
 }
 
 def sonarScan() {
