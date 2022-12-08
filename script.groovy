8884815dae975a80f25344851fd8e43848d6ba8b
Ghaida Bouchaala <ghaida.bouchaala@expensya.com>
2022-12-08 12:10:13 +0100

    fix: jenkinsfile script

--- script.groovy
+++ script.groovy
@@ -1,11 +1,11 @@
 def pushToNexus() {
     echo "pushing the jar file to Nexus maven-snapshots repo..."
-    sh 'mvn clean package -D maven.test.skip=true deploy:deploy-file -D groupId=org.springframework.samples -D artifactId=spring-petclinic -D version=2.7.3 -D packaging=jar -D repositoryId=deploymentRepo -D url=http://localhost:8081/repository/maven-releases/ -D file=target/spring-petclinic-2.7.3.jar'
+    sh 'mvn clean package -D maven.test.skip=true deploy:deploy-file -D groupId=com.example -D artifactId=demo -D version=3.0.0 -D packaging=jar -D repositoryId=deploymentRepo -D url=http://localhost:8081/repository/maven-releases/ -D file=target/demo-0.0.1-SNAPSHOT.jar'
 }
 
 def sonarScan() {
     echo "Running sonarQube scan..."
-    sh "mvn clean verify sonar:sonar -Dsonar.projectKey=demo -Dmaven.test.skip=true  -Dsonar.login=squ_594c07451d6adeedecdb514f8a2f9e9e3ae2f403"
+    sh "mvn clean verify sonar:sonar -D sonar.projectKey=devops -D maven.test.skip=true  -D sonar.login=squ_9854aec5640594c23dd308012cde3301142f63fc"
     //devops
     }
 
